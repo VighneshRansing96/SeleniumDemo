6180e0469509ac26845f0190fe8c095146fca338
Vighnesh <vransing@whitehedge.com>
2023-04-28 08:19:06 +0530

    Commit and Push

---
+++ src/test/java/demoPackage/demoClass.java
@@ -0,0 +1,12 @@
+package demoPackage;
+
+public class demoClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to GitHub World");
+		System.out.println("Hello GitHub");
+
+	}
+
+}
